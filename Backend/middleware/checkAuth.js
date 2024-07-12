40d138f965732d8636050f375c9d940938c0ea79
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-07-12 11:32:47 +0200

    ongoing dev

--- Backend/middleware/checkAuth.js
+++ Backend/middleware/checkAuth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(" ")[1];
         console.log(token);
         const decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.userData = { userId: decodedToken.userId };
+        req.userData = { userId: decoded.userId };
         next();
     } catch (error) {
         console.log(error);
