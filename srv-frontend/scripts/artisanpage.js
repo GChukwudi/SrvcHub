f8535bd9f0e117b8a6e60cae46ee7d6e4f4264a4
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-07-28 16:21:55 +0200

    updaated

--- srv-frontend/scripts/artisanpage.js
+++ srv-frontend/scripts/artisanpage.js
@@ -1,5 +1,9 @@
 
 const resultContainer = document.getElementById('profileDetails')
+const bookingform = document.getElementById('bookingForm')
+const dateInput = document.getElementById('date')
+const calendarEl = document.getElementById('calendar')
+
 
 document.addEventListener('DOMContentLoaded', async () => {
     const urlParams = new URLSearchParams(window.location.search)
@@ -35,4 +39,29 @@ document.addEventListener('DOMContentLoaded', async () => {
             alert('Failed to fetch artisan profile');
         }
     }
+
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        dateClick: function(info) {
+            dateInput.value = info.dateStr;
+            bookingform.style.display = 'block';
+        }
+    });
+
+    calendar.render();
+
+    bookingform.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const formData = new FormData(bookingform);
+        const booking = {
+            artisanId,
+            date: formData.get('date'),
+            time: formData.get('time'),
+            location: formData.get('location')
+        };
+        try {
+            
+        } catch (error) {
+            
+        }
 });
